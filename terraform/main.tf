c1d9d30b676066f50dfb946142d984353bd4cb62
asjadon <jadonsinghankit31@gmail.com>
2026-05-25 13:00:15 +0000

    Added Terraform Ansible and Kubernetes setup

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "devops_server" {
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t2.medium"
+  key_name      = "ansible-key"
+
+  tags = {
+    Name = "Flask-DevOps-Server"
+  }
+}
